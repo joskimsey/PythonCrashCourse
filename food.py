6e8b52e1b8c1b5dd4205c4a628336df4ac80d7b7
joskimsey <joskimsey@gmail.com>
2020-07-31 18:40:03 -0700

    First commit

---
+++ food.py
@@ -0,0 +1,10 @@
+# myfoods = ["meat","ugali"]
+# myfriends_foods = myfoods[:]
+# print("my favourite food is:")
+# print(myfoods)
+# print(myfriends_foods)
+
+i=0
+for i in range(0,100):
+    print("Joskimsey + {}".format(i))
+
---
+++ hello.py
@@ -0,0 +1,7 @@
+num = float(input("enter first number: "))
+op = input("enter operator: ")
+num = float(input("enter second number: "))
+if op ="+":
+    elseif op "//":
+    else:
+        print("invalid operator")
