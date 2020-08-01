7c5ceff38aa780411d4666a9c62266373bc39830
joskimsey <joskimsey@gmail.com>
2020-07-31 18:47:09 -0700

    Removed some unnecessary files

--- hello.py
+++
@@ -1,7 +0,0 @@
-num = float(input("enter first number: "))
-op = input("enter operator: ")
-num = float(input("enter second number: "))
-if op ="+":
-    elseif op "//":
-    else:
-        print("invalid operator")
